b63c1efa66db16dd9bba80b8e058b84ed340d71b
mohiuddin.ice.ru <mohiuddin.ice.ru@gmail.com>
2022-12-15 12:32:01 +0600

    profile page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Switch, Redirect, Route } from 'react-router';
-import { BrowserRouter, Link } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Redirect, Route } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import Login from './Login';
-import Register from './Register';
-import Dashboard from './Dashboard';
-import Test from './Test';
-import Profile from './Profile';
-import Enumerator from './Enumerator';
-import './Login.css';
+import Login from "./Login";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
+import Test from "./Test";
+import Profile from "./Profile";
+import Enumerator from "./Enumerator";
+import "./Login.css";
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route exact path='/' component={Login} />
-            <Route exact path='/register' component={Register} />
-            <Route path='/dashboard' component={Dashboard} />
-            <Route path='/enumerator' component={Enumerator} />
-            <Route path='/test' component={Test} />
-
-            <Route path='/profile' component={Profile} />
+            <Route exact path="/" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/enumerator" component={Enumerator} />
+            <Route path="/test" component={Test} />
+            <Route path="/profile/:id" component={Profile} />
             {/* <Route component={NotFound}/> */}
         </Switch>
     </BrowserRouter>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
