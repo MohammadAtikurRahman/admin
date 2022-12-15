b5c7657a6d09b68c10ea2047b74c7ee3c16b9617
mohiuddin.ice.ru <mohiuddin.ice.ru@gmail.com>
2022-12-15 14:59:52 +0600

    class component to function component  and useNavigate

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,33 +1,38 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import swal from "sweetalert";
 import { Button, TextField, Link } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
 var salt = bcrypt.genSaltSync(10);
 
-export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-        };
-    }
+export default function Login(props) {
+    const [user, setUser] = useState({
+        username: "",
+        password: "",
+    });
+    const navigate = useNavigate();
 
-    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+    const onChange = (event) => {
+        setUser((user) => {
+            return {
+                ...user,
+                [event.target.name]: event.target.value,
+            };
+        });
+    };
 
-    login = () => {
-        const pwd = bcrypt.hashSync(this.state.password, salt);
-
+    const login = () => {
+        const pwd = bcrypt.hashSync(user.password, salt);
         axios
             .post("http://localhost:2000/login", {
-                username: this.state.username,
+                username: user.username,
                 password: pwd,
             })
             .then((res) => {
                 localStorage.setItem("token", res.data.token);
                 localStorage.setItem("user_id", res.data.id);
-                this.props.history.push("/dashboard");
+                if (res.status === 200) navigate("/dashboard");
             })
             .catch((err) => {
                 if (
@@ -44,54 +49,49 @@ export default class Login extends React.Component {
             });
     };
 
-    render() {
-        return (
-            <div style={{ marginTop: "200px" }}>
-                <div>
-                    <h2>Login</h2>
-                </div>
-
-                <div>
-                    <TextField
-                        id="standard-basic"
-                        type="text"
-                        autoComplete="off"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.onChange}
-                        placeholder="User Name"
-                        required
-                    />
-                    <br />
-                    <br />
-                    <TextField
-                        id="standard-basic"
-                        type="password"
-                        autoComplete="off"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.onChange}
-                        placeholder="Password"
-                        required
-                    />
-                    <br />
-                    <br />
-                    <Button
-                        className="button_style"
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        disabled={
-                            this.state.username == "" &&
-                            this.state.password == ""
-                        }
-                        onClick={this.login}>
-                        Login
-                    </Button>{" "}
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <Link href="/register">Register</Link>
-                </div>
+    return (
+        <div style={{ marginTop: "200px" }}>
+            <div>
+                <h2>Login</h2>
             </div>
-        );
-    }
+
+            <div>
+                <TextField
+                    id="standard-basic"
+                    type="text"
+                    autoComplete="off"
+                    name="username"
+                    value={user.username}
+                    onChange={onChange}
+                    placeholder="User Name"
+                    required
+                />
+                <br />
+                <br />
+                <TextField
+                    id="standard-basic"
+                    type="password"
+                    autoComplete="off"
+                    name="password"
+                    value={user.password}
+                    onChange={onChange}
+                    placeholder="Password"
+                    required
+                />
+                <br />
+                <br />
+                <Button
+                    className="button_style"
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    disabled={user.username == "" && user.password == ""}
+                    onClick={login}>
+                    Login
+                </Button>{" "}
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Link href="/register">Register</Link>
+            </div>
+        </div>
+    );
 }
