c2210f8973b6c02f9361e5920297242a84ba3340
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2022-12-27 18:06:54 +0600

    local

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
@@ -20,6 +21,9 @@ const fs = require("fs");
 const product = require("./model/product.js");
 const user = require("./model/user.js");
 const jwt_decode = require("jwt-decode");
+
+
+const PORT = process.env.PORT
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -510,6 +514,9 @@ app.post("/api", async (req, res) => {
     }
 });
 
+
+
+
 // const ssss = require('crypto').randomBytes(64).toString('hex')
 // // '09f26e402586e2faa8da4c98a35f1b20d6b033c6097befa8be3486a829587fe2f90a832bd3ff9d42710a4da095a2ce285b009f0c3730cd9b8e1af3eb84df6611'
 
@@ -529,6 +536,6 @@ const payloadinit = buff.toString("ascii");
 var payload = JSON.parse(payloadinit);
 console.log(payload);
 
-app.listen(2000, () => {
+app.listen(PORT, () => {
     console.log("Server is Runing On port 2000");
 });
