b1029c0677c8c5a06d1775e0b472fbcba42b25c5
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-01-16 18:50:09 +0600

    login bug fixed

--- backend/model/user.js
+++ backend/model/user.js
@@ -63,7 +63,7 @@ const userSchema = new Schema(
         id: {type: Number, default: () => nextId++},
         userId: Number,
         username: String,
-        password: String,
+        password: Number,
         country: String,
         beneficiary: [beneficiarySchema],
     },
