699135de4dec95c6e2be234dc4d397def54245b6
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-01-23 18:38:14 +0600

    uri change

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const mongoose = require("mongoose").set("debug", false);
 const {router} = require("./routes.js");
 const {randomNumberNotInUserCollection} = require("./helpers/number");
 
-mongoose.connect(process.env.MONGO_URI_LOCAL, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
