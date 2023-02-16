5432abe75363f65694e02d687fe1aeff8a0047f4
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-02-16 13:04:00 +0600

    bug fix

--- frontend/src/Test.js
+++ frontend/src/Test.js
@@ -1438,7 +1438,7 @@ export default class Test extends Component {
                 })
 
                 .map((row, index) =>
-                row.score1 >= 0 ? (
+                row.score1 !== undefined && row.score1 !== null && row.score1 >= 0 ? (
                     <TableRow key={index}>
                       <TableCell align="center">
                         {new Date(row.updatedAt).toLocaleString("en-GB", {
