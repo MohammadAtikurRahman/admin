34f4aaed0cca5e52c95b5b3703b40ac6c17f2df6
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-07-11 13:13:32 +0600

    done

--- backend/server.js
+++ backend/server.js
@@ -421,6 +421,7 @@ app.get("/get-testscore", async (req, res) => {
 
     return res.status(200).json(extact_data);
 });
+
 app.get("/get-transaction", async (req, res) => {
     let users = await user
         .find({})
@@ -457,45 +458,6 @@ app.get("/get-transaction", async (req, res) => {
     }
 });
 
-
-
-const moment = require('moment-timezone');
-
-app.get("/get-transaction", async (req, res) => {
-  let users = await user
-    .find({})
-    .select("beneficiary")
-    .lean();
-
-  const data = users;
-
-  // Map and format data
-  const mapped_data = data.map((user) => {
-    const { beneficiary } = user;
-    return beneficiary.map((ben) => ({
-      beneficiaryId: ben.beneficiaryId,
-      name: ben.name,
-      loggedin_time: ben.loggedin_time ? moment.utc(ben.loggedin_time).tz("Asia/Dhaka").format("YYYY-MM-DD HH:mm:ss") : null,
-      transaction: ben.transaction.map((t) => ({
-        beneficiaryMobile: t.beneficiaryMobile,
-        type: t.type,
-        amount: t.amount,
-        date: t.date,
-        duration: t.duration,
-        updatedAt: t.updatedAt,
-        createdAt: t.createdAt,
-      })),
-    }));
-  }).flat().reverse();
-
-  if (mapped_data.length > 0) {
-    return res.status(200).json(mapped_data);
-  } else {
-    return res.status(404).json({
-      message: "No data found.",
-    });
-  }
-});
 
 
 
