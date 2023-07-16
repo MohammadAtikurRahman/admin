d7119cb6c2d345d87cd3b9ad4e83fa243d27dad2
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-07-17 01:38:51 +0600

    done

--- backend/controllers/beneficiaryController.js
+++ backend/controllers/beneficiaryController.js
@@ -245,11 +245,6 @@ async function beneficiaryLogin(req, res) {
 
 async function transaction(req, res) {
     req.body.forEach(transaction => {
-        // Check if beneficiaryMobile is present
-        if (!transaction.beneficiaryMobile) {
-            return res.status(400).send(`Missing beneficiaryMobile for transaction with beneficiaryId: ${transaction.beneficiaryId}`);
-        }
-
         User.findOneAndUpdate(
             {"beneficiary.beneficiaryId": transaction.beneficiaryId},
             {
@@ -276,6 +271,7 @@ async function transaction(req, res) {
     });
     res.status(201).send("Transactions added successfully");
 }
+
 
 
 // async function transaction(req, res) {
