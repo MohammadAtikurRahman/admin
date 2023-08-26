f5e3c7d285d8cf9537282baf9f8babc81b090df6
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-08-26 18:20:38 +0600

    transactiondetials i some chnage in csv download

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const user = require("./model/user.js");
 const jwt_decode = require("jwt-decode");
 
 const PORT = process.env.PORT;
-app.use(express.json({limit: "50mb"}));
+app.use(express.json({limit: "100mb"}));
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
