b6a92b99a8ed0728cb9d30d6276cbda03034bee5
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-11-02 04:00:28 +0600

    bulk limit more

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,9 @@ const user = require("./model/user.js");
 const jwt_decode = require("jwt-decode");
 
 const PORT = process.env.PORT;
-app.use(express.json({limit: "100000mb"}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
