72d8f39b2a49cdebfc229c6367651c6b73023eb6
atiktest2023 <contact.atikurrahman@gmail.com>
2024-07-03 02:24:18 +0600

    new era

--- backend/controllers/onlineController.js
+++ backend/controllers/onlineController.js
@@ -70,6 +70,64 @@
 
 const Transaction = require("../model/transaction");
 
+// async function addTransaction(req, res) {
+//     try {
+//         const phone = req.headers['phone'];
+//         const beneficiaryId = req.headers['beneficiaryid'];
+
+//         // Ensure headers are provided
+//         if (!phone || !beneficiaryId) {
+//             return res.status(400).send({
+//                 message: "Missing 'phone' or 'BeneficiaryId' header"
+//             });
+//         }
+
+//         let transactions = req.body;
+
+//         // If the body is not an array or is empty, create a default transaction
+//         if (!Array.isArray(transactions) || transactions.length === 0) {
+//             transactions = [{
+//                 beneficiaryId: Number(beneficiaryId),
+//                 beneficiaryMobile: phone,
+//                 type: "default",
+//                 amount: 0,
+//                 duration: 0,
+//                 trxid: "N/A",
+//                 sub_type: "N/A",
+//                 date: new Date().toISOString(),
+//                 duration_bkash: 0,
+//                 sender: "N/A",
+//                 duration_nagad: 0,
+//                 raw_sms: "N/A",
+//                 timestamp: new Date() // Add current timestamp
+//             }];
+//         } else {
+//             transactions = transactions.map(transactionData => ({
+//                 ...transactionData,
+//                 beneficiaryId: Number(beneficiaryId),
+//                 beneficiaryMobile: phone,
+//                 timestamp: new Date() // Add current timestamp
+//             }));
+//         }
+
+//         const transactionPromises = transactions.map(async transactionData => {
+//             const newTransaction = new Transaction(transactionData);
+//             await newTransaction.save();
+//         });
+
+//         await Promise.all(transactionPromises);
+
+//         res.status(201).send("Transactions added successfully");
+//     } catch (error) {
+//         console.error("Detailed Error:", error);
+//         res.status(400).send({
+//             message: "An error occurred while processing transactions.",
+//             error: error.message || error
+//         });
+//     }
+// }
+
+
 async function addTransaction(req, res) {
     try {
         const phone = req.headers['phone'];
@@ -126,7 +184,6 @@ async function addTransaction(req, res) {
         });
     }
 }
-
 
 async function getTransactions(req, res) {
     try {
