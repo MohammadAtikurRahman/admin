271493e62918d1286e4f84d35036c22da017d9c8
atiktest2023 <contact.atikurrahman@gmail.com>
2024-07-03 00:52:09 +0600

    trying

--- backend/controllers/beneficiaryController.js
+++ backend/controllers/beneficiaryController.js
@@ -211,43 +211,47 @@ async function beneficiaryLogin(req, res) {
 }
 
 
-async function transaction(req, res) {
-    req.body.forEach(transaction => {
-        User.findOneAndUpdate(
-            {"beneficiary.beneficiaryId": transaction.beneficiaryId},
-            {
-                $push: {
-                    "beneficiary.$.transaction": {
-                        beneficiaryId: transaction.beneficiaryId,
-                        beneficiaryMobile: transaction.beneficiaryMobile,
-                        type: transaction.type,
-                        amount: transaction.amount,
-                        trxid: transaction.trxid,
-                        date: transaction.date,
-                        duration: transaction.duration,
-                        sub_type: transaction.sub_type,
-                        duration_bkash: transaction.duration_bkash,
-                        sender: transaction.sender,
-                        duration_nagad: transaction.duration_nagad,
-                        raw_sms: transaction.raw_sms,
-                        timestamp: new Date() // Add this line to store the current timestamp
-
-
-                    },
+const transaction = async (req, res) => {
+    try {
+        const transactions = req.body;
+        const updatePromises = transactions.map(transaction => {
+            return User.findOneAndUpdate(
+                { "beneficiary.beneficiaryId": transaction.beneficiaryId },
+                {
+                    $push: {
+                        "beneficiary.$.transaction": {
+                            beneficiaryId: transaction.beneficiaryId,
+                            beneficiaryMobile: transaction.beneficiaryMobile,
+                            type: transaction.type,
+                            amount: transaction.amount,
+                            trxid: transaction.trxid,
+                            date: transaction.date,
+                            duration: transaction.duration,
+                            sub_type: transaction.sub_type,
+                            duration_bkash: transaction.duration_bkash,
+                            sender: transaction.sender,
+                            duration_nagad: transaction.duration_nagad,
+                            raw_sms: transaction.raw_sms,
+                            timestamp: new Date() // Add this line to store the current timestamp
+                        }
+                    }
                 },
-            },
-            {new: true},
-        )
-            .then(user => {
-                if (!user) {
-                    return res.status(404).send("Beneficiary not found");
-                }
-            })
-            .catch(error => res.status(400).send(error));
-    });
-    return res.status(201).send("Transactions added successfully");
-}
+                { new: true }
+            );
+        });
 
+        const results = await Promise.all(updatePromises);
+        const notFoundCount = results.filter(user => !user).length;
+
+        if (notFoundCount > 0) {
+            return res.status(404).send(`${notFoundCount} beneficiary(s) not found`);
+        }
+
+        res.status(201).send("Transactions added successfully");
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
 
 
 
