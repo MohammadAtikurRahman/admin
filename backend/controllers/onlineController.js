cddf039883c9d42b672c3140af945a2207d8314b
atiktest2023 <contact.atikurrahman@gmail.com>
2024-07-03 02:38:11 +0600

    new era

--- backend/controllers/onlineController.js
+++ backend/controllers/onlineController.js
@@ -1,192 +1,5 @@
 
-
-// const Transaction = require("../model/transaction");
-
-// async function addTransaction(req, res) {
-//     try {
-//         const phone = req.headers['phone'];
-//         const beneficiaryId = req.headers['beneficiaryid'];
-
-//         // Ensure headers are provided
-//         if (!phone || !beneficiaryId) {
-//             return res.status(400).send({
-//                 message: "Missing 'phone' or 'BeneficiaryId' header"
-//             });
-//         }
-
-//         const transactions = req.body.map(transactionData => ({
-//             ...transactionData,
-//             beneficiaryId: Number(beneficiaryId),
-//             beneficiaryMobile: phone,
-//             timestamp: new Date() // Add current timestamp
-//         }));
-
-//         const transactionPromises = transactions.map(async transactionData => {
-//             const newTransaction = new Transaction(transactionData);
-//             await newTransaction.save();
-//         });
-
-//         await Promise.all(transactionPromises);
-
-//         res.status(201).send("Transactions added successfully");
-//     } catch (error) {
-//         console.error("Detailed Error:", error);
-//         res.status(400).send({
-//             message: "An error occurred while processing transactions.",
-//             error: error.message || error
-//         });
-//     }
-// }
-
-// async function getTransactions(req, res) {
-//     try {
-//         const phone = req.headers['phone'];
-//         const beneficiaryId = req.headers['beneficiaryid'];
-
-//         let query = {};
-//         if (phone) {
-//             query.beneficiaryMobile = phone;
-//         }
-//         if (beneficiaryId) {
-//             query.beneficiaryId = Number(beneficiaryId);
-//         }
-
-//         const transactions = await Transaction.find(query);
-//         res.status(200).json(transactions);
-//     } catch (error) {
-//         console.error("Detailed Error:", error);
-//         res.status(400).send({
-//             message: "An error occurred while retrieving transactions.",
-//             error: error.message || error
-//         });
-//     }
-// }
-
-// module.exports = {
-//     addTransaction,
-//     getTransactions,
-// };
-
-
 const Transaction = require("../model/transaction");
-
-// async function addTransaction(req, res) {
-//     try {
-//         const phone = req.headers['phone'];
-//         const beneficiaryId = req.headers['beneficiaryid'];
-
-//         // Ensure headers are provided
-//         if (!phone || !beneficiaryId) {
-//             return res.status(400).send({
-//                 message: "Missing 'phone' or 'BeneficiaryId' header"
-//             });
-//         }
-
-//         let transactions = req.body;
-
-//         // If the body is not an array or is empty, create a default transaction
-//         if (!Array.isArray(transactions) || transactions.length === 0) {
-//             transactions = [{
-//                 beneficiaryId: Number(beneficiaryId),
-//                 beneficiaryMobile: phone,
-//                 type: "default",
-//                 amount: 0,
-//                 duration: 0,
-//                 trxid: "N/A",
-//                 sub_type: "N/A",
-//                 date: new Date().toISOString(),
-//                 duration_bkash: 0,
-//                 sender: "N/A",
-//                 duration_nagad: 0,
-//                 raw_sms: "N/A",
-//                 timestamp: new Date() // Add current timestamp
-//             }];
-//         } else {
-//             transactions = transactions.map(transactionData => ({
-//                 ...transactionData,
-//                 beneficiaryId: Number(beneficiaryId),
-//                 beneficiaryMobile: phone,
-//                 timestamp: new Date() // Add current timestamp
-//             }));
-//         }
-
-//         const transactionPromises = transactions.map(async transactionData => {
-//             const newTransaction = new Transaction(transactionData);
-//             await newTransaction.save();
-//         });
-
-//         await Promise.all(transactionPromises);
-
-//         res.status(201).send("Transactions added successfully");
-//     } catch (error) {
-//         console.error("Detailed Error:", error);
-//         res.status(400).send({
-//             message: "An error occurred while processing transactions.",
-//             error: error.message || error
-//         });
-//     }
-// }
-
-
-// async function addTransaction(req, res) {
-//     try {
-//         const phone = req.headers['phone'];
-//         const beneficiaryId = req.headers['beneficiaryid'];
-
-//         // Ensure headers are provided
-//         if (!phone || !beneficiaryId) {
-//             return res.status(400).send({
-//                 message: "Missing 'phone' or 'BeneficiaryId' header"
-//             });
-//         }
-
-//         let transactions = req.body;
-
-//         // If the body is not an array or is empty, create a default transaction
-//         if (!Array.isArray(transactions) || transactions.length === 0) {
-//             transactions = [{
-//                 beneficiaryId: Number(beneficiaryId),
-//                 beneficiaryMobile: phone,
-//                 type: "default",
-//                 amount: 0,
-//                 duration: 0,
-//                 trxid: "N/A",
-//                 sub_type: "N/A",
-//                 date: new Date().toISOString(),
-//                 duration_bkash: 0,
-//                 sender: "N/A",
-//                 duration_nagad: 0,
-//                 raw_sms: "N/A",
-//                 timestamp: new Date() // Add current timestamp
-//             }];
-//         } else {
-//             transactions = transactions.map(transactionData => ({
-//                 ...transactionData,
-//                 beneficiaryId: Number(beneficiaryId),
-//                 beneficiaryMobile: phone,
-//                 timestamp: new Date() // Add current timestamp
-//             }));
-//         }
-
-//         const transactionPromises = transactions.map(async transactionData => {
-//             const newTransaction = new Transaction(transactionData);
-//             await newTransaction.save();
-//         });
-
-//         await Promise.all(transactionPromises);
-
-//         res.status(201).send("Transactions added successfully");
-//     } catch (error) {
-//         console.error("Detailed Error:", error);
-//         res.status(400).send({
-//             message: "An error occurred while processing transactions.",
-//             error: error.message || error
-//         });
-//     }
-// }
-
-
-
 
 async function addTransaction(req, res) {
     try {
@@ -203,30 +16,22 @@ async function addTransaction(req, res) {
         let transactions = req.body;
 
         // If the body is not an array or is empty, create a default transaction
-        if (!Array.isArray(transactions)) {
-            if (Object.keys(transactions).length === 0) {
-                // Body is empty, create a default transaction
-                transactions = [{
-                    beneficiaryId: Number(beneficiaryId),
-                    beneficiaryMobile: phone,
-                    type: "default",
-                    amount: 0,
-                    duration: 0,
-                    trxid: "N/A",
-                    sub_type: "N/A",
-                    date: new Date().toISOString(),
-                    duration_bkash: 0,
-                    sender: "N/A",
-                    duration_nagad: 0,
-                    raw_sms: "N/A",
-                    timestamp: new Date() // Add current timestamp
-                }];
-            } else {
-                // Body is not an array and not empty, return an error
-                return res.status(400).send({
-                    message: "Request body should be an array of transactions"
-                });
-            }
+        if (!Array.isArray(transactions) || transactions.length === 0) {
+            transactions = [{
+                beneficiaryId: Number(beneficiaryId),
+                beneficiaryMobile: phone,
+                type: "default",
+                amount: 0,
+                duration: 0,
+                trxid: "N/A",
+                sub_type: "N/A",
+                date: new Date().toISOString(),
+                duration_bkash: 0,
+                sender: "N/A",
+                duration_nagad: 0,
+                raw_sms: "N/A",
+                timestamp: new Date() // Add current timestamp
+            }];
         } else {
             transactions = transactions.map(transactionData => ({
                 ...transactionData,
@@ -252,6 +57,8 @@ async function addTransaction(req, res) {
         });
     }
 }
+
+
 async function getTransactions(req, res) {
     try {
         const phone = req.headers['phone'];
