bcc1b59d62918ad4d3bdc27cb6fdb2fd923db558
atiktest2023 <contact.atikurrahman@gmail.com>
2024-07-02 16:21:50 +0600

    ok

--- backend/controllers/beneficiaryController.js
+++ backend/controllers/beneficiaryController.js
@@ -210,43 +210,81 @@ async function beneficiaryLogin(req, res) {
 }
 
 
+// async function transaction(req, res) {
+//     req.body.forEach(transaction => {
+//         User.findOneAndUpdate(
+//             {"beneficiary.beneficiaryId": transaction.beneficiaryId},
+//             {
+//                 $push: {
+//                     "beneficiary.$.transaction": {
+//                         beneficiaryId: transaction.beneficiaryId,
+//                         beneficiaryMobile: transaction.beneficiaryMobile,
+//                         type: transaction.type,
+//                         amount: transaction.amount,
+//                         trxid: transaction.trxid,
+//                         date: transaction.date,
+//                         duration: transaction.duration,
+//                         sub_type: transaction.sub_type,
+//                         duration_bkash: transaction.duration_bkash,
+//                         sender: transaction.sender,
+//                         duration_nagad: transaction.duration_nagad,
+//                         raw_sms: transaction.raw_sms,
+//                         timestamp: new Date() // Add this line to store the current timestamp
+
+
+//                     },
+//                 },
+//             },
+//             {new: true},
+//         )
+//             .then(user => {
+//                 if (!user) {
+//                     return res.status(404).send("Beneficiary not found");
+//                 }
+//             })
+//             .catch(error => res.status(400).send(error));
+//     });
+//     return res.status(201).send("Transactions added successfully");
+// }
+
 async function transaction(req, res) {
-    req.body.forEach(transaction => {
-        User.findOneAndUpdate(
-            {"beneficiary.beneficiaryId": transaction.beneficiaryId},
-            {
-                $push: {
-                    "beneficiary.$.transaction": {
-                        beneficiaryId: transaction.beneficiaryId,
-                        beneficiaryMobile: transaction.beneficiaryMobile,
-                        type: transaction.type,
-                        amount: transaction.amount,
-                        trxid: transaction.trxid,
-                        date: transaction.date,
-                        duration: transaction.duration,
-                        sub_type: transaction.sub_type,
-                        duration_bkash: transaction.duration_bkash,
-                        sender: transaction.sender,
-                        duration_nagad: transaction.duration_nagad,
-                        raw_sms: transaction.raw_sms,
-                        timestamp: new Date() // Add this line to store the current timestamp
-
-
-                    },
+    try {
+        const promises = req.body.map(async transaction => {
+            const user = await User.findOneAndUpdate(
+                {"beneficiary.beneficiaryId": transaction.beneficiaryId},
+                {
+                    $push: {
+                        "beneficiary.$.transaction": {
+                            beneficiaryId: transaction.beneficiaryId,
+                            beneficiaryMobile: transaction.beneficiaryMobile,
+                            type: transaction.type,
+                            amount: transaction.amount,
+                            trxid: transaction.trxid,
+                            date: transaction.date,
+                            duration: transaction.duration,
+                            sub_type: transaction.sub_type,
+                            duration_bkash: transaction.duration_bkash,
+                            sender: transaction.sender,
+                            duration_nagad: transaction.duration_nagad,
+                            raw_sms: transaction.raw_sms,
+                            timestamp: new Date() // Add this line to store the current timestamp
+                        }
+                    }
                 },
-            },
-            {new: true},
-        )
-            .then(user => {
-                if (!user) {
-                    return res.status(404).send("Beneficiary not found");
-                }
-            })
-            .catch(error => res.status(400).send(error));
-    });
-    return res.status(201).send("Transactions added successfully");
-}
+                {new: true}
+            );
+            if (!user) {
+                throw new Error("Beneficiary not found");
+            }
+        });
 
+        await Promise.all(promises);
+
+        res.status(201).send("Transactions added successfully");
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
 
 
 
