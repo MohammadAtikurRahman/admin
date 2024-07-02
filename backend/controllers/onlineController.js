67b5dcfd3df3b0538e10a415fd29db6bbf034f5c
atiktest2023 <contact.atikurrahman@gmail.com>
2024-07-03 01:59:57 +0600

    new era

--- backend/controllers/onlineController.js
+++ backend/controllers/onlineController.js
@@ -2,27 +2,6 @@
 
 // const Transaction = require("../model/transaction");
 
-// // async function transaction(req, res) {
-// //     try {
-// //         const transactions = req.body;
-
-// //         const transactionPromises = transactions.map(async transactionData => {
-// //             const newTransaction = new Transaction(transactionData);
-// //             await newTransaction.save();
-// //         });
-
-// //         await Promise.all(transactionPromises);
-
-// //         res.status(201).send("Transactions added successfully");
-// //     } catch (error) {
-// //         console.error("Detailed Error:", error);
-// //         res.status(400).send({
-// //             message: "An error occurred while processing transactions.",
-// //             error: error.message || error
-// //         });
-// //     }
-// // }
-
 // async function addTransaction(req, res) {
 //     try {
 //         const phone = req.headers['phone'];
@@ -59,9 +38,6 @@
 //     }
 // }
 
-
-
-
 // async function getTransactions(req, res) {
 //     try {
 //         const phone = req.headers['phone'];
@@ -86,10 +62,9 @@
 //     }
 // }
 
-
 // module.exports = {
 //     addTransaction,
-//     getTransactions
+//     getTransactions,
 // };
 
 
