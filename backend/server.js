88cf1be4ac937c88a1b12bbf7c386dead0c83441
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-08-16 10:00:33 +0600

    port taken from env

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const product = require("./model/product.js");
 const user = require("./model/user.js");
 const jwt_decode = require("jwt-decode");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true}));
 
@@ -2507,7 +2507,7 @@ app.get("/check-ids", async (req, res) => {
 
 
 
-app.listen(2000, (err, data) => {
+app.listen(PORT, (err, data) => {
     // console.log(err);
-    console.log("Server is Runing On port 2000");
+    console.log(`Server is Runing On port ${PORT}`);
 });
