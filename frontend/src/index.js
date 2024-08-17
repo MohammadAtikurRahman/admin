4a24a4c6357c93dbe12a2f745459375e35c804f1
Mohiuddin <mohiuddin.ice.ru@gmail.com>
2024-08-17 16:08:45 +0600

    transaction details not required

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,25 +8,22 @@ import Test from "./Test";
 import Notest from "./Notest";
 import Profile from "./Profile";
 import Transaction from "./Transaction";
-import Transactiondetails from "./Transactiondetails"
 import Enumerator from "./Enumerator";
 import "./Login.css";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/enumerator" element={<Enumerator />} />
-      <Route path="/test" element={<Test />} />
-      <Route path="/notest" element={<Notest />} />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/enumerator" element={<Enumerator />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/notest" element={<Notest />} />
 
-      <Route path="/profile/:id" element={<Profile />} />
-      <Route path="/transaction/:id" element={<Transaction />} />
-
-      <Route path="/transactiondetails" element={<Transactiondetails />} />
-    </Routes>
-  </BrowserRouter>,
-  document.getElementById("root")
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/transaction/:id" element={<Transaction />} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
